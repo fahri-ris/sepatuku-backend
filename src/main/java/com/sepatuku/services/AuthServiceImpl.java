4afa65a1a8f3be532ffb7b39e963314a873041a0
fahri-ris <fahri.ris15@gmail.com>
2023-11-21 20:26:19 +0700

    fix implement method

--- src/main/java/com/sepatuku/services/AuthServiceImpl.java
+++ src/main/java/com/sepatuku/services/AuthServiceImpl.java
@@ -2,6 +2,7 @@ package com.sepatuku.services;
 
 import com.sepatuku.dtos.MessageResponseDto;
 import com.sepatuku.dtos.auth.LoginRequestDto;
+import com.sepatuku.dtos.auth.RegisterRequestDto;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +10,11 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public MessageResponseDto login(LoginRequestDto loginRequestDto) {
+        return null;
+    }
+
+    @Override
+    public MessageResponseDto register(RegisterRequestDto registerRequestDto) {
         return null;
     }
 }
